a19f3fdc90ed49b8022ef6469f2d33d20435def7
Isabelle <loariyafiaba@gmail.com>
2023-08-24 00:37:21 +0100

    Working on the dark theme + theme button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-var playerNameInput = document.querySelector('#playerNameInput')
-const startQuizBtn = document.querySelector('#startQuizBtn');
+var playerNameInput = document.querySelector("#playerNameInput");
+const startQuizBtn = document.querySelector("#startQuizBtn");
 
-startQuizBtn.addEventListener('click', function() {
+startQuizBtn.addEventListener("click", function () {
+  // Get the name and set it to localStorage for late use
+  var setPlayerName = playerNameInput.value;
+  localStorage.setItem("playerName", setPlayerName);
+});
 
-    // Get the name and set it to localStorage for late use
-    var setPlayerName = playerNameInput.value;
-    localStorage.setItem('playerName', setPlayerName);
-    
-  });
-  
+// const darkModeButton = document.getElementById("darkModeButton");
+// darkModeButton.addEventListener("click", () => {
+//   document.documentElement.classList.toggle("dark-mode");
+// });
+
+
+const modesToggle = document.getElementById("modesToggle");
+modesToggle.addEventListener("click", function () {
+  var mode = modesToggle.dataset.mode;
+
+  if (mode === "light") {
+    document.documentElement.classList.toggle("dark-mode");
+    modesToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    modesToggle.dataset.mode = "dark";
+  } else if (mode === "dark") {
+    document.documentElement.classList.toggle("dark-mode");
+    modesToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    modesToggle.dataset.mode = "light";
+  }
+});
