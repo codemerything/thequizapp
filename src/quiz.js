709a644d97c7332aa419c25503c12b212723518f
devdot <hi@devdot.me>
2023-08-28 14:23:28 +0100

    commented :]

--- src/quiz.js
+++ src/quiz.js
@@ -13,18 +13,20 @@ var questionText = document.querySelector("#question");
 var answerBoxes = document.querySelectorAll(".choice-text");
 
 scoreText.textContent = score;
+// the above is kept from before
 
-var allQuestions = [];
+var allQuestions = []; //setting up an empty array to hold class instance objects
+
 var QuestionClass = class {
     constructor(questionNumber, question, answerList, rightAnswer) {
         this.questionNumber = questionNumber;
         this.question = question;
         this.answerList = answerList;
         this.rightAnswer = rightAnswer;
-        allQuestions.push(this);
+        allQuestions.push(this); //add the new question object to the end of array
     }
 
-    displayQuestion() {
+    displayQuestion() { // this method is the same as the old one
         questionNumberText.textContent = "Question " + this.questionNumber;
         questionText.textContent = this.question;
 
@@ -35,7 +37,7 @@ var QuestionClass = class {
         }
 
     }
-    checkQuestion(answerBox) {
+    checkQuestion(answerBox) { // i have moved the on click check into the method
         console.log(answerBox.textContent)
 
         if (answerBox.textContent == this.rightAnswer) {
@@ -48,7 +50,7 @@ var QuestionClass = class {
 
 
             setTimeout(() => {
-                answerBox.style.pointerEvents = "auto";
+                answerBox.style.pointerEvents = "auto"; //returns clicks back to normal on that answerbox
             }, 1000);
         } else if (answerBox.textContent != this.rightAnswer) {
 
@@ -65,10 +67,10 @@ new QuestionClass(2, "What is the capital of France?", ["London", "Paris", "Madr
 new QuestionClass(3, "What is the largest mammal on Earth?", ["Elephant", "Giraffe", "Blue Whale", "Rhinoceros"], "Blue Whale");
 
 
-var clickedBox;
+var clickedBox;//holds the element clicked
 var correct = false;
 
-function getClick(i) {
+function getClick(i) {// get click checks if a click has happened on an answer box. it is async because in order to use a for loop to iterate through the questions, you need it to await a click
     return new Promise(acc => {
         function handleClick() {
             setTimeout(() => {
@@ -76,17 +78,17 @@ function getClick(i) {
                     answerBox.removeEventListener('click', handleClick);
                     if (correct = true) {
                         correct = false;
-                        acc();
+                        acc(); //fulfill promise
                     }
                 });
-            }, 1000);
+            }, 1000); //wait one second before continuing
 
         }
         answerBoxes.forEach((answerBox) => {
-            answerBox.addEventListener('click', () => {
-                clickedBox = answerBox;
-                check(i);
-                handleClick()
+            answerBox.addEventListener('click', () => { //apply event listener for each answe box
+                clickedBox = answerBox; //sets the clicked element globally
+                check(i);// checks the question
+                handleClick()// calls the other function as to fullful the promise and move on
             });
         });
     });
@@ -94,18 +96,17 @@ function getClick(i) {
 
 
 function check(i) {
-    console.log(i)
-    allQuestions[i].checkQuestion(clickedBox);
+    allQuestions[i].checkQuestion(clickedBox);//calling method of the current question object
 }
 
 async function main() {
-    for (let i = 0; i < allQuestions.length; i++) {
+    for (let i = 0; i < allQuestions.length; i++) {//for loop that starts off by displaying the question, then calls get click
 
         allQuestions[i].displayQuestion();
 
         await getClick(i);
-        console.log("click received", i);
+
     }
-    console.log("done");
+
 }
 main();
