1bf453a6bc7514ebbe453e132b6a2bf253190111
mmnldm <mremmanueledem@gmail.com>
2023-09-01 23:41:48 +0100

    feat: 🎸 added selection page + logic
    i added the selection page and dynamically inserted the categories for
    users to pick from
    
    BREAKING CHANGE: 🧨 1. need to fix styling on the page

---
+++ src/select-quiz.js
@@ -0,0 +1,59 @@
+const selectionHeader = document.getElementById('selection-header');
+const selected = document.getElementById('selected');
+let getName = localStorage.getItem("playerName");
+const playerName = document.getElementById('playerName');
+
+playerName.textContent = getName;
+
+const questionsUrl = './src/quiz-games.json';
+
+function getQuizTitles() {
+    fetch(questionsUrl)
+      .then((response) => response.json())
+      .then((quizzes) => {
+        // Loop through each quiz ID (e.g., "quiz1", "quiz2")
+        for (const quizId in quizzes) {
+            if (quizzes.hasOwnProperty(quizId)) {
+                const quiz = quizzes[quizId];
+                const title = quiz.title;
+
+                let titleText = document.createElement('button');
+                titleText.classList.add(`${quiz.quizId}`, "selectButton", "selectOption");
+
+                titleText.textContent = title;
+                selectionHeader.append(titleText);
+            }
+        }
+        select();
+      });
+  }
+
+  function select() {
+
+    const selectButton = document.querySelectorAll('.selectButton');
+    let highlightButton = null;
+
+    selectButton.forEach((button) => {
+        button.addEventListener('click', () => {
+            if(highlightButton){
+                highlightButton.classList.remove('highlighted');
+            }
+    
+            button.classList.add('highlighted');
+            highlightButton = button;
+        })
+      })
+    
+      document.addEventListener('keypress', (e) => {
+        if(e.key === "Enter" && highlightButton){
+            localStorage.setItem('quizid', highlightButton.className[0]);
+            location.href = "quiz.html";
+        }
+      })
+    
+  }
+
+  
+
+getQuizTitles();
+
